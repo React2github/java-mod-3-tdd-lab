54d593ddc06560b8124ce0aced7c3a3d5fac3385
Abdella <abdella.shukur2@gmail.com>
2022-07-12 20:50:53 -0400

    first set of tests are passing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,14 @@ public class Main {
     public static void main(String[] args) {
         // your code here
     }
+
+    public static String fizzBuzzString(String str) {
+        if (str == "") return null;
+        if (str == "f") return "Fizz";
+        if (str == "b") return "Buzz";
+        if(str.startsWith("f") && str.endsWith("b")) return "FizzBuzz";
+        return str;
+    }
+
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,9 +2,41 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Array;
+
 class MainTest {
 
     @Test
     void main() {
     }
+
+    @Test 
+    void fizzBuzzTestForNull() {
+        String response = Main.fizzBuzzString("");
+        assertEquals(null, response);
+    }
+
+    @Test 
+    void fizzBuzzTestForF() {
+        String response = Main.fizzBuzzString("f");
+        assertEquals("Fizz", response);
+    }
+
+    @Test 
+    void fizzBuzzTestForB() {
+        String response = Main.fizzBuzzString("b");
+        assertEquals("Buzz", response);
+    }
+
+    @Test 
+    void fizzBuzzTestForFAndB() {
+        String response = Main.fizzBuzzString("fb");
+        assertEquals("FizzBuzz", response);
+    }
+
+    // @Test
+    // void fizzBuzzArray() {
+    // Array resultsArr = 
+    // assertArrayEquals(expectedArr.toArray(), resultsArr.toArray());
+    // }
 }
